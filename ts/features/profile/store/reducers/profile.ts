b7eb13f1d4edb2eb48059d2dce450543d404b6c9
Mario Perrotta <mario.perrotta@pagopa.it>
2022-06-03 16:52:12 +0200

    feat: added new profile main screen

--- ts/features/profile/store/reducers/profile.ts
+++ ts/features/profile/store/reducers/profile.ts
@@ -1,9 +1,13 @@
 import { getType } from "typesafe-actions";
 import * as pot from "italia-ts-commons/lib/pot";
+import { createSelector } from "reselect";
+import { fromNullable, none, Option } from "fp-ts/lib/Option";
 import { InitializedProfile } from "../../../../../definitions/backend/InitializedProfile";
 import { Action } from "../../../../store/actions/types";
 import { getErrorFromNetworkError } from "../../../../utils/errors";
 import { getProfile } from "../actions";
+import { GlobalState } from "../../../../store/reducers/types";
+import { EmailAddress } from "../../../../../definitions/backend/EmailAddress";
 
 export type ProfileState = pot.Pot<InitializedProfile, Error>;
 
@@ -23,5 +27,48 @@ const profileReducer = (
   }
   return state;
 };
+
+export const profileSelector = (state: GlobalState): ProfileState =>
+  state.profile;
+
+export const profileFullNameSelector = createSelector(
+  profileSelector,
+  (profile: ProfileState): Option<string> =>
+    pot.getOrElse(
+      pot.map(profile, p =>
+        fromNullable(p.name).chain(n =>
+          fromNullable(p.family_name).map(f => `${n} ${f}`)
+        )
+      ),
+      none
+    )
+);
+
+export const profileBirthDateSelector = createSelector(
+  profileSelector,
+  (profile: ProfileState): Option<Date> =>
+    pot.getOrElse(
+      pot.map(profile, p => fromNullable(p.date_of_birth)),
+      none
+    )
+);
+
+export const profileFiscalCodeSelector = createSelector(
+  profileSelector,
+  (profile: ProfileState): Option<string> =>
+    pot.getOrElse(
+      pot.map(profile, p => fromNullable(p.fiscal_code)),
+      none
+    )
+);
+
+export const profileEmailSelector = createSelector(
+  profileSelector,
+  (profile: ProfileState): Option<EmailAddress> =>
+    pot.getOrElse(
+      pot.map(profile, p => fromNullable(p.email)),
+      none
+    )
+);
 
 export default profileReducer;
