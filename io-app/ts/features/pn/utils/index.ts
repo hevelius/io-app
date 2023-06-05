993589d3dd64d8c6c11bd00aca24a3dd30277dc6
Mario Perrotta <mario.perrotta@pagopa.it>
2023-06-05 11:15:09 +0200

    chore: update

--- io-app/ts/features/pn/utils/index.ts
+++
@@ -1,8 +0,0 @@
-import I18n from "../../../i18n";
-import { NotificationStatus } from "../store/types/types";
-
-export function getNotificationStatusInfo(status: NotificationStatus) {
-  return I18n.t(`features.pn.details.timeline.status.${status}`, {
-    defaultValue: status
-  });
-}
--- io-app/ts/store/actions/preferences.ts
+++
@@ -1,9 +0,0 @@
-import { ActionType, createStandardAction } from "typesafe-actions";
-
-export const preferencesLanguagesLoadSuccess = createStandardAction(
-  "PREFERENCES_LANGUAGES_LOAD_SUCCESS"
-)<ReadonlyArray<string>>();
-
-export type PreferencesActions = ActionType<
-  typeof preferencesLanguagesLoadSuccess
->;
